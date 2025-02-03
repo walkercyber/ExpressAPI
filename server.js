c3e91aeae6a6573ddfe7b5011c174a482258f5ce
walkercyber <isabella.daood@iths.se>
2025-02-03 21:17:58 +0100

    CRUD operations done

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express ();
+app.use(express.json());
+const PORT = 3000;
+
+const NET_API_URL ="http://localhost:5041"
+
+app.get("/products", async (req, res) => { 
+  try {
+      const response = await globalThis.fetch(NET_API_URL + "/products");
+      res.json(await response.json());
+  } catch (error) {
+      res.status(500).json({ error: "Unable to retrieve data" });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+      const { name, type } = req.body;
+      if (!name || !type) return res.status(400).json({ error: "Some required fields are missing" });
+
+      console.log("Sending to .NET API:", req.body);
+
+      const response = await fetch(NET_API_URL + "/products", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(req.body)
+      });
+
+      res.json({ message: "Data sent", response: await response.json() });
+  } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "Unable to submit data" });
+  }
+});
+
+  app.listen(PORT, () => {
+    console.log("listening to 3000");
+});
